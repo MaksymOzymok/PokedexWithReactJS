5bfa6ceaf14875f5c02e559ab0dcdeba478eef9f
MaxOzymok <ozymokmax1@ukr.net>
2020-05-02 22:48:39 +0300

    Updates

---
+++ precache-manifest.19a49ec8b77b27b987083eb6aacdc3ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2660d0dc21b005a396ca39ef10503804",
+    "url": "/PokedexWithReactJS/index.html"
+  },
+  {
+    "revision": "ef393c7f8d09358b3271",
+    "url": "/PokedexWithReactJS/static/css/main.07e518fc.chunk.css"
+  },
+  {
+    "revision": "0ab74804d98bbbc83be5",
+    "url": "/PokedexWithReactJS/static/js/2.1d656683.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/PokedexWithReactJS/static/js/2.1d656683.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef393c7f8d09358b3271",
+    "url": "/PokedexWithReactJS/static/js/main.568c676b.chunk.js"
+  },
+  {
+    "revision": "0b5942d475cebf662cf0",
+    "url": "/PokedexWithReactJS/static/js/runtime-main.94b3563d.js"
+  }
+]);
